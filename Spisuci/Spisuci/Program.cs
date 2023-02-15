4d4c285014493084badc7831b83b886ea0f8d294
andyEmilowa <andyowaa@gmail.com>
2023-02-15 09:32:58 +0200

    completing task

---
+++ Spisuci/Spisuci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spisuci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int last = nums[nums.Count - 1];
+            nums.RemoveAll(x => x == last);
+            Console.WriteLine(string.Join(" ", nums));
+            
+        }
+    }
+}
