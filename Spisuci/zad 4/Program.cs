edf5bf9dc0d54c8cc57e1d99d30b55cdba9638a1
andyEmilowa <andyowaa@gmail.com>
2023-02-22 09:21:01 +0200

    zad 4

---
+++ Spisuci/zad 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace zad_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string command = input[0];
+                if (command == "Print")
+                {
+                    switch(command)
+                    {
+                        case "add": var index = int.Parse(input[1]);
+                            var element = int.Parse(input[2]);
+                            nums.Insert(index, element);
+                            break;
+
+                        case "addMany":
+
+                            break;
+
+                        case "contains":
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
