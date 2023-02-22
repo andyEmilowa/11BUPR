d02a66edc71b1e4b3964caf60cb744bb75d44804
andyEmilowa <andyowaa@gmail.com>
2023-02-22 08:44:27 +0200

    zad 3

---
+++ Spisuci/Zad 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> result = new List<int>();
+            List<string> input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+            input.Reverse();
+            for (int i = 0; i < input.Count; i++)
+            {
+                int[] temp = input[i].Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                result.AddRange(temp);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
